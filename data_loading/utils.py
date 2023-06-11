31da4d7e08511c887a79682a72f0f4746001f097
llt <wolilitian@gmail.com>
2023-06-11 11:47:35 +0200

    fs transformation

--- data_loading/utils.py
+++ data_loading/utils.py
@@ -28,6 +28,17 @@ test_csv_path_all_sample = "../preprocess/audio/all_sample/"
 txt_path = "../preprocess/audio/output_file/"
 
 
+
+balloon_pop_1_video_frame = 23030 # to
+balloon_pop_1_accel_frame = 45977 + 19/34
+
+balloon_pop_2_video_frame = 74844
+balloon_pop_2_accel_frame = 47706 + 23/28
+
+balloon_pop_3_video_frame = 166836.5
+balloon_pop_3_accel_frame = 50776 + 30.5/32
+
+
 def reset_examples_ids(examples):
     for i, ex in enumerate(examples):
         ex['id'] = i
@@ -320,3 +331,13 @@ class Maker():
                 continue
 
             new_examples.append(ex)
+
+
+video_seconds_to_accel_sample = interp1d(
+    [
+        balloon_pop_1_video_frame/29.97,
+        balloon_pop_3_video_frame/29.97
+    ], [
+        balloon_pop_1_accel_frame,
+        balloon_pop_3_accel_frame
+    ], fill_value="extrapolate")
